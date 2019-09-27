ee9fa1b6c289cb004d6037a71fd6fba236b167de
高岩 <826395862@qq.com>
2019-09-27 21:54:37 +0800

    exercise 1_1 restruct

--- chapter_1/src/exercises1_1.py
+++ chapter_1/src/exercises1_1.py
@@ -2,8 +2,8 @@
 your runing time about different N. '''
 import time
 import random
-import exercises1_1_package
-from exercises1_1_package.bubble import BubbleSort
+from exercises1_1_package import bubble
+from exercises1_1_package import algorithm2
                         
 k = int( input("Please input the postion k(0<k<10001):") )
 N = k * 2
@@ -15,20 +15,21 @@ int_list_algorithm2_origin = int_list_bubble_origin[:]
 '''Choice implement in BubbleSort'''
 time_cost = time.clock()
 
-BubbleSort(int_list_bubble_origin)
+bubble.BubbleSort(int_list_bubble_origin)
 kth_element_in_list = int_list_bubble_origin[k-1]
 
 time_cost = time.clock() - time_cost
 
-exercises1_1_package.bubble.BubblePrintResult(kth_element_in_list, time_cost)
+bubble.BubblePrintResult(kth_element_in_list, time_cost)
 
 '''Choice implement in algorithm2:Firstly,Sort the first k elements. Then compare the 
 latter element with the first k element.'''
 time_cost = time.clock()
 
-exercises1_1_package.algorithm2.Algorithm2(int_list_algorithm2_origin)
+algorithm2.Algorithm2(int_list_algorithm2_origin)
 kth_element_in_list_algorithm2 = int_list_algorithm2_origin[k-1]
 
 time_cost = time.clock() - time_cost
 
-exercises1_1_package.algorithm2.Algorithm2PrintResult(kth_element_in_list, time_cost)
+algorithm2.Algorithm2PrintResult(kth_element_in_list, time_cost)
+
--- chapter_1/src/exercises1_1_package/__init__.py
+++ chapter_1/src/exercises1_1_package/__init__.py
@@ -3,8 +3,8 @@ Created on 2019年9月26日
 define Bubble sort function and PrintResult function
 @author: gaoyan
 '''
-from exercises1_1_package.bubble import *
-from exercises1_1_package.algorithm2 import *
+from exercises1_1_package import *
+
 
 
 
